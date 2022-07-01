e1d8009fe845a81a7f17addf42f632614419e9d6
Obradovic Nikola <obradovicnikola009@gmail.com>
2022-07-01 17:12:29 +0200

    login page handle form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLoginInterface } from 'src/app/models/dto/UserLoginInterface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router, private userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onLoginSubmit: (userLogin: UserLoginInterface) => void = (
+    userLogin: UserLoginInterface
+  ) => {
+    this.userService.login(userLogin).subscribe({
+      next: (response) => {
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/']);
+        }
+      },
+      error: (error) => {
+        console.log('Error...');
+        console.log(error);
+      },
+    });
+  };
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserLoginInterface } from '../models/dto/UserLoginInterface';
+import { TransactionResponseInterface } from '../models/entities/TransactionResponseInterface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  private expensesUrl = 'http://127.0.0.1:8081/api';
+  constructor(private http: HttpClient) {}
+
+  config = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
+  getExpenses(): Observable<TransactionResponseInterface[]> {
+    return this.http.get<TransactionResponseInterface[]>(this.expensesUrl);
+  }
 
   isLoggedIn() {
     if (localStorage.getItem('token')) return true;
     return false;
+  }
+
+  login(userLogin: UserLoginInterface): Observable<any> {
+    return this.http.post(
+      `${this.expensesUrl}/auth/login`,
+      userLogin,
+      this.config
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
   }
 }
