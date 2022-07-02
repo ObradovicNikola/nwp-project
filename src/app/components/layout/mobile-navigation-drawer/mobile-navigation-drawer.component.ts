bc4d52e0a135e43fa3b33c6004cde25c87e55b8c
Obradovic Nikola <obradovicnikola009@gmail.com>
2022-07-02 12:11:34 +0200

    mobile drawer with security

--- src/app/components/layout/mobile-navigation-drawer/mobile-navigation-drawer.component.ts
+++ src/app/components/layout/mobile-navigation-drawer/mobile-navigation-drawer.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { accountNavigationItems } from 'src/app/config/accountNavigationItems';
 import { navigationItems } from 'src/app/config/navigationItems';
 import { NavigationItemInterface } from 'src/app/models/layout/NavigationItemInterface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-mobile-navigation-drawer',
@@ -15,7 +16,15 @@ export class MobileNavigationDrawerComponent implements OnInit {
   navigationItems: NavigationItemInterface[] = navigationItems;
   accountNavigationItems: NavigationItemInterface[] = accountNavigationItems;
 
-  constructor() {}
+  constructor(private userService: UserService) {}
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLoggedIn;
+  }
+
+  get username(): string {
+    return this.userService.username;
+  }
 
   ngOnInit(): void {}
 
@@ -26,5 +35,9 @@ export class MobileNavigationDrawerComponent implements OnInit {
 
   stopPropagation(e: Event): void {
     e.stopPropagation();
+  }
+
+  logout(): void {
+    this.userService.logout();
   }
 }
